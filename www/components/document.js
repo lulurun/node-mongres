20f653dfdbfa2197fa70ece1d4585b68feca0105
Xiaolu Liu <xiaolu.liu@gree.net>
2014-08-26 17:28:49 +0900

    qjeury autosize

--- www/components/document.js
+++ www/components/document.js
@@ -10,32 +10,35 @@ F(function(){
     },
     afterRender: function(cb){
       var self = this;
-      self.$("#btn-save").click(function(){
-        var text = self.$("#input-doc").val();
-        text = text.replace(
-            /: ObjectID\('([a-f0-9]{24})'\)(,?)\n/g,
-          function(str, m1, m2){
-            return ": \"ObjectID('" + m1 + "')\"" + m2 + "\n";
-          }
-        );
-        var doc = JSON.parse(text);
-        MONGRES.client.saveDoc(doc, function(res){
-          if (res.length === 1 && res[0]._id) {
-            F.navigate("document", {
-              conn: F.env.conn,
-              db: F.env.db,
-              col: F.env.col,
-              doc: res[0]._id.substr(10, 24)
-            });
-          } else {
-            self.load();
-          }
+      F.require("bower_components/jquery-autosize/jquery.autosize.min.js", function(){
+        self.$("#input-doc").autosize();
+        self.$("#btn-save").click(function(){
+          var text = self.$("#input-doc").val();
+          text = text.replace(
+              /: ObjectID\('([a-f0-9]{24})'\)(,?)\n/g,
+            function(str, m1, m2){
+              return ": \"ObjectID('" + m1 + "')\"" + m2 + "\n";
+            }
+          );
+          var doc = JSON.parse(text);
+          MONGRES.client.saveDoc(doc, function(res){
+            if (res.length === 1 && res[0]._id) {
+              F.navigate("document", {
+                conn: F.env.conn,
+                db: F.env.db,
+                col: F.env.col,
+                doc: res[0]._id.substr(10, 24)
+              });
+            } else {
+              self.load();
+            }
+          });
         });
+        self.$("#btn-reset").click(function(){
+          self.$("#input-doc").val(self.data.data);
+        });
+        cb();
       });
-      self.$("#btn-reset").click(function(){
-        self.$("#input-doc").val(self.data.data);
-      });
-      cb();
     },
     getData: function(cb){
       var self = this;
@@ -57,15 +60,13 @@ F(function(){
           );
 
           self.data = {
-            data: text,
-            height: $(window).height() - 200
+            data: text
           };
           cb();
         });
       } else {
           self.data = {
-            data: "{\n\n}",
-            height: $(window).height() - 200
+            data: "{\n\n}"
           };
         cb();
       }
